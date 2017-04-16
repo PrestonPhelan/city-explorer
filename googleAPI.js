420c73565e8f0bd369dbb490f87dfcd3e8ba75f8
PrestonPhelan <preston.phelan@gmail.com>
2017-04-16 14:52:12 -0700

    Multiple API calls at once

--- googleAPI.js
+++ googleAPI.js
@@ -19,16 +19,34 @@ const getMedValue = parsed => {
 const getCityRelativeValue = (cityString, query) => {
   getAllResults(query).then(result => {
     const parsedJSON = JSON.parse(result).default.geoMapData;
-    console.log(parsedJSON);
+    // console.log(parsedJSON);
     const median = getMedValue(parsedJSON);
-    console.log(median);
+    // console.log(median);
     const city = parsedJSON.find( element => element.geoName === cityString );
-    console.log(city);
+    // console.log(city);
     const cityValue = city.value[0];
-    console.log(cityValue);
-    console.log(cityValue / median);
+    // console.log(cityValue);
+    // console.log(cityValue / median);
     return cityValue / median;
   });
 };
 
-module.exports = getCityRelativeValue;
+const makeApiCalls = queries => {
+  let results = new Object;
+  let queriesLeft = queries.length;
+  queries.forEach( query => {
+    getAllResults(query).then(result => {
+      results[query] = JSON.parse(result).default.geoMapData;
+      --queriesLeft;
+      if (queriesLeft <= 0) {
+        console.log("Done with queries");
+        console.log(results);
+        return results;
+      } else {
+          console.log("Still waiting...");
+        }
+    });
+  });
+};
+
+module.exports = makeApiCalls;
