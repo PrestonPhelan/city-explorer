d61200c5bf30105c9f52db0db03e972b7ffad6e2
PrestonPhelan <preston.phelan@gmail.com>
2017-04-16 17:59:20 -0700

    Async API calls, then sort cities

--- googleAPI.js
+++ googleAPI.js
@@ -1,11 +1,17 @@
 const googleTrends = require('google-trends-api');
 
 const getAllResults = query => {
+  // Basic API Call to Google Trends, gets data by metro area
   return googleTrends.interestByRegion({
     keyword: query,
     geo: 'US',
     resolution: 'DMA'
   });
+};
+
+const parseResults = rawResults => {
+  // Takes the raw results, return an array of objects
+  return JSON.parse(rawResults).default.geoMapData;
 };
 
 const getMedValue = parsed => {
@@ -49,4 +55,73 @@ const makeApiCalls = queries => {
   });
 };
 
-module.exports = makeApiCalls;
+const sendAllQueries = queries => {
+  let results = new Object;
+  const totalLength = queries.length;
+
+  const sendNextQuery = (queryList, sendTo, totalQueries) => {
+    let query = queryList[0];
+    console.log(`Starting query ${query}`);
+    getAllResults(query).then( result => {
+      console.log(`Got results for ${query}`);
+      let parsed = parseResults(result);
+      results[query] = parsed;
+      if (queryList.length > 1) {
+        sendNextQuery(queryList.slice(1), sendTo, totalQueries);
+      } else {
+        sendTo(results, totalQueries);
+      }
+      return;
+    });
+  };
+
+  sendNextQuery(queries, sortResults, totalLength);
+};
+
+const sortResults = (results, numQueries) => {
+  console.log("Sorting results...");
+  let cities = new Object;
+  Object.keys(results).forEach(query => {
+    console.log(`Getting median for ${query}`);
+    let median = getMedValue(results[query]);
+    results[query].forEach(city => {
+      console.log(`Calculating value for ${city.geoName}`);
+      if (!cities[city.geoName]) {
+        cities[city.geoName] = new Object;
+        cities[city.geoName].name = city.geoName;
+        cities[city.geoName].values = [];
+      }
+      cities[city.geoName].values.push(city.value[0] / median);
+    });
+  });
+
+  console.log(cities);
+
+  let cityRatings = [];
+  Object.keys(cities).forEach( city => {
+    console.log(`Calculating score for ${cities[city].name}`);
+    let sum = cities[city].values.reduce( (acc, cur) => acc + cur, 0 );
+    cityRatings.push([cities[city].name, sum / numQueries]);
+  });
+
+  console.log("Unsorted city ratings:");
+  console.log(cityRatings);
+
+  cityRatings.sort( (a, b) => b[1] - a[1] );
+  console.log("Sorted city ratings");
+  console.log(cityRatings);
+  return cityRatings;
+};
+//
+// const getCityRankings = queries => {
+//   let calls = new Promise( (resolve, reject) {
+//     makeApiCalls(queries)
+//   })
+//   let results = makeApiCalls(queries);
+//   while (data === undefined) {
+//     console.log("Waiting on data...");
+//     sl
+//   }
+// };
+
+module.exports = sendAllQueries;
